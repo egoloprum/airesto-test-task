3e612627b51fc3425db35a512bca4fee03c9f88e
egoloprum <bayraa093@gmail.com>
2025-08-10 01:33:58 +0300

    ui: add date segment control

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { DateSegmentControl } from '@/features/segmentControls'
+
 export default function Home() {
-  return <main></main>
+  return (
+    <main className="px-8 py-16">
+      <h1 className="text-white text-2xl">Бронирования</h1>
+      <DateSegmentControl />
+    </main>
+  )
 }
--- src/features/(navbar)/ui/searchInput.tsx
+++ src/features/(navbar)/ui/searchInput.tsx
@@ -9,7 +9,7 @@ export const SearchInput = ({}) => {
     <div className="relative w-72 text-white border-2 border-stone-700 rounded-md">
       <Search className="absolute top-1/2 -translate-y-1/2 left-2 w-4 h-4 text-gray-400" />
       <Input
-        className="border-none w-full bg-stone-800 pl-8 h-8"
+        className="border-none w-full bg-stone-900 rounded-md pl-8 h-8"
         placeholder="⌘+Л поиск по имени"
       />
     </div>
